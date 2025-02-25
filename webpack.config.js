39bf95d6b37eaffd10ae201fdcf307d1a6f3e503
xxl-13 <minglong.wu@student.fh-kiel.de>
2025-02-25 22:03:45 +0800

    fix2

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
-import HtmlWebPackPlugin from "html-webpack-plugin";
-import { resolve as _resolve } from 'path';
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
-export const entry = "./src/index.jsx";
-export const output = {
-  path: _resolve(__dirname, 'dist'),
-  filename: 'bundle.js',
-  publicPath: '/'
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-    },
-    {
-      test: /\.html$/,
-      use: [
-        {
-          loader: "html-loader",
-        },
-      ],
-    },
-    {
-      test: /\.css$/,
-      use: [
-        {
-          loader: "style-loader",
-        },
-        {
-          loader: "css-loader",
-        },
-      ],
-    }
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
   ],
 };
-export const resolve = {
-  extensions: ["*", ".js", ".jsx"],
-};
-export const devServer = {
-  historyApiFallback: true,
-};
-export const plugins = [
-  new HtmlWebPackPlugin({
-    template: "./public/index.html",
-    filename: "./index.html",
-  }),
-];
